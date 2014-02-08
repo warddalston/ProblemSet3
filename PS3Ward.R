8693cc7d1d28cc3cdc4483d4f4959e78c3e4f312
warddalston <ward.dalston@gmail.com>
2014-02-08 10:04:13 -0600

    Section A, Problem 1
    Create a document and write out code for the first problem.

---
+++ PS3Ward.R
@@ -0,0 +1,19 @@
+################################################
+### PS3 - Dalston Ward - February 2014       ###
+################################################
+
+#Start everything by emptying the workspace.  (don't use this if concurrently working on other projects in R!)
+rm(list=ls())
+#Second, set the working directory as appropriate.  This makes the code cleaner and helps to keep track of any saved files.
+setwd("~/Documents/WashU 2nd Year/Applied Stats Programming/Feb 6/PS3/ProblemSet3/")
+#Third, set the seed for replicability
+set.seed(1801)
+
+# Section A: Sampling Distributions and P-Values
+
+# 1. Make a three dimensional array with dim=c(20,5,100) and fill it with random data.
+filler <- rpois(20*5*1000,10) #The random data is sampled from a Poisson Distribution
+data <- array(filler,dim=c(20,5,1000)) # It is then stuck into an array of appropriate size
+rm(filler) #remove the filler data, just to keep the workspace clean
+
+
